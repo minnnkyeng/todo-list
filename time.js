815c7235d3555f7143b5d2738b69a3ec235134bd
minkyeng <minnn.xyz@gmail.com>
2020-03-03 23:06:29 +0900

    현재 년월일 보여주기

--- time.js
+++ time.js
@@ -1,4 +1,14 @@
 const currentTime = document.getElementById('time');
+const currentDate = document.getElementById('date');
+
+function getToday(){
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDay();
+
+    currentDate.innerText = `${year}.${month<10?`0${month}`:month}.${day<10?`0${day}`:day}`;
+}
 
 function getTime(){
     const time = new Date();
@@ -6,10 +16,11 @@ function getTime(){
     const minute = time.getMinutes()<10 ? "0"+time.getMinutes() : time.getMinutes();
     const second = time.getSeconds()<10 ? "0"+time.getSeconds() : time.getSeconds();
         
-    currentTime.innerText = hour+" : "+minute+" : "+second;
+    currentTime.innerText = `${hour<10?`0${hour}`:hour}:${minute<10?`0${minute}`:minute}:${second<10?`0${second}`:second}`;
 }
 
 function init(){
+    getToday();
     setInterval(getTime,1000);
 }
 
